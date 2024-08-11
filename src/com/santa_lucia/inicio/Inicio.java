1a82262289b8c3831bfef90ee03e12df096f9bf8
Zaid <zahidparedesmerino57@gmail.com>
2024-08-11 00:57:48 -0500

    Bienvenido(a)

--- src/com/santa_lucia/inicio/Inicio.java
+++ src/com/santa_lucia/inicio/Inicio.java
@@ -2,10 +2,10 @@ package com.santa_lucia.inicio;
 import java.util.Scanner;
 public class Inicio {
 	public static void main(String[] args) {
-Scanner lector=new Scanner(System.in);
+Scanner lector = new Scanner(System.in);
 System.out.println("Ingrese su numero:");
 int num=lector.nextInt();
-System.out.println("El valor es: "+num);
+System.out.println("El valor nuvo para github: "+num);
 }
 }
 
